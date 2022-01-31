cf8cc0b227be7b76284ac798a0dceaa770551308
Katherine Yuko Fry <97481293+katherineyukofry@users.noreply.github.com>
2022-01-31 10:48:52 -0800

    Create test-sketch.js

---
+++ libraries/test-sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+   background(220);
+}
+
+function draw() {
+ellipse(200,240,200,50);
+strokeWeight(0);
+fill(255,166,201);
+rect(200,100,100,140);
+rect(100,100,100,140);
+strokeWeight(1);
+ellipse(200,100,200,50);
+}
